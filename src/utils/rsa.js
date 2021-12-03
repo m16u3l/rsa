eeb15f0e7cf819e348336f34007219637682915d
Miguel Condori <m16u3l_777@hotmail.com>
2021-12-03 00:31:04 -0400

    rsa con modificacaiones

--- src/utils/rsa.js
+++ src/utils/rsa.js
@@ -4,15 +4,13 @@ import { mcd } from "./utils";
 import bigInt from "big-integer";
 
 let calcularD = (e, fN) => {
-  return bigInt(e).modInv(bigInt(fN))
-  /*
+  //return bigInt(e).modInv(bigInt(fN))
   e %= fN;
   for (let i = 1; i < fN; i++) {
     if ((e * i) % fN === 1) {
       return i;
     }
   }
-  */
 }
 
 let calcularE = (fN) => {
@@ -44,23 +42,38 @@ let generarParDeClaves = () => {
   return new KeyPair(clavePublica, clavePrivada);
 }
 
-let encriptarRSA = (mensajeEnClaro, clavePublica) => {
+const encriptarRSA = (mensajeEnClaro, clavePublica) => {
   const [es, ns] = clavePublica.split(',');
   const e = bigInt(es);
   const n = bigInt(ns);
-  // encriptar
-  let mensajeEncriptado = bigInt(mensajeEnClaro).modPow(e, n);
-  return mensajeEncriptado;
+
+  const arrayEncriptados = [];
+
+  for (let i = 0; i < mensajeEnClaro.length; i++) {
+    let ascii = mensajeEnClaro.charCodeAt(i);
+    // encriptar
+    let charencripted = bigInt(ascii).modPow(e, n);
+    arrayEncriptados.push(charencripted);
+  }
+
+  // let mensajeEncriptado = bigInt(mensajeEnClaro).modPow(e, n);
+  return arrayEncriptados;
 }
 
 let desencriptarRSA = (mensajeEncriptado, clavePrivada) => {
   const [ds, ns] = clavePrivada.split(',');
+
   const d = bigInt(ds);
   const n = bigInt(ns);
 
-  // desencriptar
-  const x = bigInt(mensajeEncriptado).modPow(d, n);
-  const mensajeDesencriptado = x;
+  let mensajeDesencriptado = "";
+  const arrayEncriptados = mensajeEncriptado.split(",");
+  for (let i = 0; i < arrayEncriptados.length; i++) {
+    // desencriptar
+    const ascii = bigInt(arrayEncriptados[i]).modPow(d, n);
+    mensajeDesencriptado += String.fromCharCode(ascii);
+  }
+
   return mensajeDesencriptado;
 }
 
